8e62643749ca2bbc80d4b194958f6ca3ae3832d9
Amir-Abdullah <69315783+Amir-Abdullah@users.noreply.github.com>
2024-10-07 00:00:52 +0600

    frontend env setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,13 @@ import { DifferenceCard } from "./card/difference";
 import { StatCard } from "./card/revCard";
 
 function App() {
-
+  console.log(import.meta.env.VITE_DEPLOYED_SERVER_URL);
   return (
     <>
       <div className="h-screen flex items-center justify-center">
         <CardWithForm />
       </div>
-      
+
       <StatCard />
       <DifferenceCard />
     </>
